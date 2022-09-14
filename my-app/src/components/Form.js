0e6320c922ee558932d55ef13cacfc16fa0a702a
xP4ST3x <81394640+xP4ST3x@users.noreply.github.com>
2022-09-14 09:03:23 -0300

    tyjhtrhtr5

--- my-app/src/components/Form.js
+++ my-app/src/components/Form.js
@@ -1,9 +1,17 @@
+import Valores from "./Valores";
+import CitySelector from "react-city-selector";
 export default function Form() {
     return (
         <>
             <div class="form-group">
-                <input type="text" class="form-control myinp" id="exampleFormControlInput1" placeholder="Ingrese la ciudad"/>
+                <input type="text" class="form-control myinp" id="exampleFormControlInput1" placeholder="Ingrese la ciudad" />
             </div>
+            <select class="custom-select myin">
+                <option selected>Seleccione un pais</option>
+                <Valores></Valores>
+            </select>
+
+            <button type="button" class="btn btn-success myi">Buscar pais</button>
         </>
     );
 }
--- my-app/src/components/Valores.js
+++ my-app/src/components/Valores.js
@@ -1,7 +1,39 @@
-export default function Valores(){
-    return(
+import React, { useEffect, useState } from "react";
+const axios = require('axios');
+
+const Valores = (props) => {
+    const [datos, setDatos] = useState([]);
+    console.log(props.id)
+    let ghostList = [];
+    useEffect(() => {
+        axios.get(`https://restcountries.com/v3.1/all`)
+        .then(function (response) {
+            console.log(response.data[0].name);
+            ghostList=(response.data)
+        })
+        .catch(function (error) {
+            console.log("No se han traido los datos de :( " + props.name)
+            console.log(error);
+        })
+        .then(function () {
+            console.log("Finally:")
+            console.log(ghostList)
+            setDatos(ghostList);
+        })
+    }, [props.id])
+    return (
         <>
-            
+            {
+                datos.map(
+                    (i) => (
+                        <>
+                            <option value={i.translations.cym.official}>{i.translations.spa.official}</option>
+                        </>
+                    )
+                )
+            }
         </>
     );
 }
+
+export default Valores;
